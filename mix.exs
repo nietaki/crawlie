01e89722284dff312f80cd03e1b4ac2f8cf704a4
Jacek Królikowski <nietaki@gmail.com>
2016-12-11 22:36:26 +0000

    trying out travis / coveralls

--- mix.exs
+++ mix.exs
@@ -2,13 +2,23 @@ defmodule Crawlie.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :crawlie,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     package: package(),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :crawlie,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      
+      package: package(),
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.html": :test,
+        "test": :test,
+      ],
+   ]
   end
 
   # Configuration for the OTP application
@@ -29,10 +39,12 @@ defmodule Crawlie.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
+      {:gen_stage, "~> 0.9.0"},
+      {:httpoison, "~> 0.10.0"},
+      # testing and documentation
       {:ex_doc, "~> 0.14.3", only: :dev},
       {:inch_ex, "~> 0.5.5", only: :dev},
-      {:httpoison, "~> 0.10.0"},
-      {:gen_stage, "~> 0.9.0"},
+      {:excoveralls, "~> 0.4", only: :test},
     ]
   end
 
