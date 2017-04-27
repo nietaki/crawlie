6169e71c9313a54dfc45f95ac43974e9a50133d9
Jacek Królikowski <nietaki@gmail.com>
2017-04-27 23:33:12 +0100

    Adding some docs and doctests to the `Crawlie.Response` module

--- lib/crawlie/response.ex
+++ lib/crawlie/response.ex
@@ -29,7 +29,19 @@ defmodule Crawlie.Response do
   #===========================================================================
 
   @spec new(String.t | URI.t, integer, [{String.t, String.t}], binary) :: This.t
+  @doc """
+  Constructs the `Crawlie.Response` struct.
 
+  ## Example
+      iex> alias Crawlie.Response
+      iex> url = "https://foo.bar/"
+      iex> headers = [{"Content-Type", "text/plain"}]
+      iex> response = Response.new(url, 200, headers, "body")
+      iex> response.body
+      "body"
+      iex> response.status_code
+      200
+  """
   def new(url, status_code, headers, body)
   when is_binary(url) do
     uri = URI.parse(url)
@@ -48,14 +60,26 @@ defmodule Crawlie.Response do
 
 
   @spec url(This.t) :: String.t
-
+  @doc """
+  Returns the string representation of the `uri` contained in the `Crawlie.Response` struct.
+  """
   def url(this) do
     URI.to_string(this.uri)
   end
 
 
   @spec content_type(This.t) :: String.t | nil
+  @doc """
+  Retrieves the content type of the response.
 
+  ## Example
+      iex> alias Crawlie.Response
+      iex> url = "https://foo.bar/"
+      iex> headers = [{"Content-Type", "text/html; charset=UTF-8"}]
+      iex> response = Response.new(url, 200, headers, "<html />")
+      iex> Response.content_type(response)
+      "text/html; charset=UTF-8"
+  """
   def content_type(%This{headers: headers}) do
     headers
     |> Enum.find_value({nil, nil}, fn {k, v} ->
@@ -69,7 +93,18 @@ defmodule Crawlie.Response do
 
 
   @spec content_type_simple(This.t) :: String.t | nil
+  @doc """
+  Retrieves the content type of the response, just the "type/subtype" part, with
+  no additional parameters, if there are any in the Content-Type header value.
 
+  ## Example
+      iex> alias Crawlie.Response
+      iex> url = "https://foo.bar/"
+      iex> headers = [{"Content-Type", "text/html; charset=UTF-8"}]
+      iex> response = Response.new(url, 200, headers, "<html />")
+      iex> Response.content_type_simple(response)
+      "text/html"
+  """
   def content_type_simple(%This{} = this) do
     case content_type(this) do
       nil -> nil
@@ -79,6 +114,5 @@ defmodule Crawlie.Response do
         |> hd()
     end
   end
-
 
 end
